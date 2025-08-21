c18b63ce396425704536e1ccd39be5888b4e6c07
Andrés Bulacia <andresbulacia040@gmail.com>
2025-08-21 11:34:03 +0200

    fixed

--- src/services/database.ts
+++ src/services/database.ts
@@ -14,3 +14,15 @@ export async function runMigrations() {
     const {migrateAddStatus} = await import('../database/0.sql');
     await migrateAddStatus();
 }
+
+export async function getAllTasksOrdered() {
+    const res = await executeSql(`
+        SELECT * FROM tasks
+        ORDER BY CASE status WHEN 'todo' THEN 1 WHEN 'doing' THEN 2 WHEN 'done' THEN 3 ELSE 4 END, created_at DESC;
+        `);
+    return res.rows._array;
+}
+
+export async function updateTaskStatus(id:number, status: string) {
+    await executeSql(`UPDATE tasks SET status = ? WHERE id = ?`, [status, id]);
+}
