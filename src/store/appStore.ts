bbd78259115a03bedc54a7974481c5ccb12c4e3e
Andrés Bulacia <andresbulacia040@gmail.com>
2025-08-21 11:32:15 +0200

    f: add loadtasks and setstatus actions

--- src/store/appStore.ts
+++ src/store/appStore.ts
@@ -1,3 +1,7 @@
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
+import { getAllTasksOrdered, updateTaskStatus } from "../services/database";
+
 export type TaskStatus = 'todo' | 'doing' | 'done';
 
 export interface Task {
@@ -9,3 +13,30 @@ export interface Task {
     user_id?: number;
     created_at?: number;
 }
+
+type State = {
+    tasks: Task[];
+    setTasks: (tasks: Task[]) => void;
+    loadTasks: () => Promise<void>;
+    setStatus: (id:number, status:TaskStatus) => Promise<void>;
+};
+
+export const useAppStore = create(
+    persist<State>(
+        (set) => ({
+            tasks: [],
+            setTasks: (tasks) => set ({tasks}),
+            loadTasks: async () => {
+                const tasks = await getAllTasksOrdered();
+                set({tasks});
+            },
+            setStatus: async (id, status) => {
+                await updateTaskStatus(id, status);
+
+                const tasks = await getAllTasksOrdered();
+                set({tasks});
+            },
+        }),
+        {name: 'app-storage'}
+    )
+);
